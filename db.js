a7e715f2b99e640ab933cdd3fe44c880ea85ffcc
Jani Inkala <t9inja00@students.oamk.fi>
2021-02-27 18:43:46 +0200

    address

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool ({
     user: 'postgres',
     password: 'Postgresnoot1401',
-    host: 'localhost',
+    host: 'https://web-interfaces-2021.herokuapp.com/',
     port: 5432,
     database: 'Web-interfaces-2021'
 });
